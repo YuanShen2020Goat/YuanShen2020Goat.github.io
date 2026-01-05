0aa206330be4a929f0e27c433d9aa0e5eea28a2c
爱门 <njfl8989@gmail.com>
2026-01-05 14:08:45 -0600

    1/5/2026
    lets player have the ability to control the camera and lock the mouse in the center of the screen (tab as switch to unlock), and object follows the camera's direction rather than the world's direction (also uploaded some assests for later working)

--- Godot 3D Game/3d-game/scripts/camera_control.gd
+++ Godot 3D Game/3d-game/scripts/camera_control.gd
@@ -1,10 +1,13 @@
 extends SpringArm3D
 
 @export var mouse_sensitivity: float = 0.01
+@export var jump_velocity = 4.5
+
+@export var camera: Camera3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	input.mouse_mode = 
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,8 +15,15 @@ func _process(delta: float) -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion: #camera follows mouse
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED: #camera follows mouse
 		var mouse_delta = event.relative
 		rotation.y -= mouse_delta.x * mouse_sensitivity
 		rotation.x -= mouse_delta.y * mouse_sensitivity
 		rotation.x = clamp(rotation.x, -PI/2, PI/4) #strict camera angle prevent over turning
+
+	if event is InputEventKey:
+		if event.keycode == KEY_TAB and event.pressed:
+			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			else:
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- Godot 3D Game/3d-game/scripts/player.gd
+++ Godot 3D Game/3d-game/scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 @export var speed = 5.0
 @export var jump_velocity = 4.5
 
+@export var camera: Camera3D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed

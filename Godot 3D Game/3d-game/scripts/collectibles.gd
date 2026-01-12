d00c004c30d1ed3029147c72abd8defb227f4d2d
爱门 <njfl8989@gmail.com>
2026-01-12 14:10:34 -0600

    1/12/2026
    improves on collectibles, added collision shape and random generate 3 types of collectibles, not complete yet for collectibles

--- Godot 3D Game/3d-game/scripts/collectibles.gd
+++ Godot 3D Game/3d-game/scripts/collectibles.gd
@@ -1,5 +1,12 @@
 extends Area3D
 
+enum CollectibleType {DIAMOND, COIN, CHERRY}
+@export var type: CollectibleType
+
+@export var diamond_model: PackedScene
+@export var coin_model: PackedScene
+@export var cherry_model: PackedScene
+
 @export var rotation_speed: float = 0.5
 @export var floating_speed: float = 0.005 #speed the object floats up and down
 @export var floating_magnitude: float = 0.05 #lowest and highest that the object can move
@@ -8,9 +15,28 @@ var original_y : float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	original_y = position.y
-
+	
+	type = randi_range(0, 2)
+	var model: PackedScene
+	match type:
+		CollectibleType.DIAMOND:
+			model = diamond_model
+		CollectibleType.COIN:
+			model = coin_model
+		CollectibleType.CHERRY:
+			model = cherry_model
+		_:
+			printerr('Invailid type!')
+			
+	var node = model.instantiate()
+	add_child(node)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void: #let collectibles rotate itself
 	rotation.y += rotation_speed * delta
 	position.y = original_y + sin(Time.get_ticks_msec() * floating_speed) * floating_magnitude
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		queue_free() # Replace with function body.
